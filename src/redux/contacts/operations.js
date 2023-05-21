e0c0f5f9f83ecc1ef9f218883520742e86126a59
Alex <103459735+torn80beta@users.noreply.github.com>
2023-05-21 14:20:15 +0300

    delete contact

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,21 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://645fbeaefe8d6fb29e255cd7.mockapi.io/api/v1';
-
-// export const fetchContacts = createAsyncThunk(
-//   'contacts/fetchAll',
-//   async (_, thunkAPI) => {
-//     try {
-//       const response = await axios.get('/contacts');
-//       //   console.log(response);
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (_, thunkAPI) => {
@@ -29,23 +14,10 @@ export const fetchContacts = createAsyncThunk(
   }
 );
 
-// export const addContact = createAsyncThunk(
-//   'contacts/addContact',
-//   async (data, thunkAPI) => {
-//     try {
-//       const response = await axios.post(`/contacts`, data);
-//       //   console.log(response);
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
 export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (data, thunkAPI) => {
-    console.log(data);
+    // console.log(data);
     try {
       const response = await axios.post(`/contacts`, data);
       //   console.log(response);
@@ -55,19 +27,6 @@ export const addContact = createAsyncThunk(
     }
   }
 );
-
-// export const deleteContact = createAsyncThunk(
-//   'contacts/deleteContact',
-//   async (id, thunkAPI) => {
-//     try {
-//       const response = await axios.delete(`/contacts/${id}`);
-//       //   console.log(response);
-//       return response.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
 
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
