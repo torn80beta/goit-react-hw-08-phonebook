6b28ae7d37b8529bb647239b806d055820fc7a4e
Alex <103459735+torn80beta@users.noreply.github.com>
2023-05-25 23:50:04 +0300

    user menu css

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,18 +1,21 @@
 import { useDispatch } from 'react-redux';
 import { userLogout } from 'redux/auth/operations';
 import { useAuth } from 'hooks';
-import css from './UserMenu.module.css';
+import { StyledUserMenuWrapper } from './UserMenu.styled';
+// import css from './UserMenu.module.css';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
 
   return (
-    <div className={css.wrapper}>
-      <p className={css.username}>Welcome, {user.name}</p>
+    <StyledUserMenuWrapper>
+      {/* // <div className={css.wrapper}> */}
+      {/* <p className={css.username}>{user.name}</p> */}
+      <p>{user.name}</p>
       <button type="button" onClick={() => dispatch(userLogout())}>
         Logout
       </button>
-    </div>
+    </StyledUserMenuWrapper>
   );
 };
---
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const StyledUserMenuWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 16px;
+`;
